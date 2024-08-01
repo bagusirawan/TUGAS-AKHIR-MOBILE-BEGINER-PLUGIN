11b1e45a01bc84f05243412bf2cff80feb404e5b
bagusirawan <bagusurawan1234@gmail.com>
2024-08-01 20:28:40 +0700

    SOAL NO 2

---
+++ src/NO1-4/NO4.kt
@@ -0,0 +1,19 @@
+package `NO1-4`
+
+fun main() {
+    val baris = 6
+
+    for (i in baris downTo 1) {
+        for (j in 1..i) {
+            print("* ")
+        }
+        println()
+    }
+
+    for (i in 1..baris) {
+        for (j in 1..i) {
+            print("* ")
+        }
+        println()
+    }
+}
